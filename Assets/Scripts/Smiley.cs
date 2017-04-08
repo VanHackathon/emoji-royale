d367b02dc5bd0d06c1199bf86714bc6daf25cccf
Bruno Paulo <bruno@eve.local>
2017-04-08 23:45:55 +0200

    instantiating initial smileys

---
+++ Assets/Scripts/Smiley.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Smiley : MonoBehaviour {
+
+	// private GameObject gameObject;
+	// private string name;
+
+	public Smiley(GameObject go, string name){
+		// gameObject = go;
+		// this.name = name;
+	}
+}
