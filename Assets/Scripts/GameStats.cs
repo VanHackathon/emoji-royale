4f668f4fbdce9ff0c54287745b1b0726d45c246f
Bruno Moreira <brucostam@Brunos-MacBook-Pro.local>
2017-04-09 00:57:23 -0300

    creating endscene. refactoring mobamanager to correctly playagain

---
+++ Assets/Scripts/GameStats.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameStats : MonoBehaviour {
+
+	public static GameStats instance = null;
+
+	public gameStatus result = gameStatus.win;
+	public int monstersKilled = 0;
+	public int timePlayedInSec = 0;
+	public int totalPower = 0;
+
+	void Awake(){
+		if (instance == null)
+			instance = this;
+		else if (instance != this)
+			Destroy(gameObject);
+		DontDestroyOnLoad(gameObject);
+	}
+
+}
