f22fb68ca874d7e4d44752ce494ce355f32bebb0
Bruno Moreira <brucostam@Brunos-MacBook-Pro.local>
2017-04-08 22:41:17 -0300

    adding explosion effect

---
+++ Assets/Scripts/DestroyAfterSecs.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyAfterSecs : MonoBehaviour {
+
+	public float delay = 2.0f; //This implies a delay of 2 seconds.
+
+	// Use this for initialization
+	void Start () 
+	{
+		Destroy (gameObject, delay);
+	}
+}
